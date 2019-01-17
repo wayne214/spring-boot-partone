d1b3a75d2c72460b6785fcb8cafb7face2e9932a
wayne <jun_wei_like1@163.com>
2019-01-17 17:09:56 +0800

    spring boot和jsp进行交互

--- src/main/java/com/wayne/partone/partone/controller/HelloWorldController.java
+++ src/main/java/com/wayne/partone/partone/controller/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 public class HelloWorldController {
-    @RequestMapping(value = "/index",method = RequestMethod.GET)
+//    @RequestMapping(value = "/index",method = RequestMethod.GET)
     public String index() {
         return "Hello World!";
     }
