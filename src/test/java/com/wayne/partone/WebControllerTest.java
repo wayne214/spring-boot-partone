c87411d30ccad9bd6659fbbaba62627f2aa30533
wayne <jun_wei_like1@163.com>
2019-01-29 15:24:17 +0800

    springboot--bug

--- src/test/java/com/wayne/partone/WebControllerTest.java
+++ src/test/java/com/wayne/partone/WebControllerTest.java
@@ -1,11 +1,9 @@
 package com.wayne.partone;
 
-import com.wayne.partone.partone.controller.HelloWorldController;
+import com.wayne.partone.web.WebController;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -14,12 +12,11 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 @SpringBootTest
 public class WebControllerTest {
 
-
     private MockMvc mockMvc;
 
     @Before
     public void setUp() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(new WebController()).build();
     }
     @Test
     public void getUser() throws Exception {
