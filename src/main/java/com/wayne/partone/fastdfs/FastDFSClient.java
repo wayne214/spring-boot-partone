4fbaa15f71f663f4a6fad22fdee8a0855c204fd1
wayne <jun_wei_like1@163.com>
2019-02-12 11:24:45 +0800

    SpringBoot--FastDFS轻量级分布式文件系统-文件上传

--- src/main/java/com/wayne/partone/fastdfs/FastDFSClient.java
+++ src/main/java/com/wayne/partone/fastdfs/FastDFSClient.java
@@ -5,7 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.csource.fastdfs.*;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * FastDFS文件系统客户端
@@ -31,7 +33,7 @@ public class FastDFSClient {
     public static String[] upload(FastDFSFile file) {
         logger.info("File Name: " + file.getName() + "File Length:" + file.getContent().length);
 
-        // 文件属性信息
+        // 文件属性信息, NameValuePair，主要存储文件的一些基础属性，如作者信息、创建时间等；
         NameValuePair[] meta_list = new NameValuePair[1];
         meta_list[0] = new NameValuePair("author", file.getAuthor());
 
@@ -40,7 +42,9 @@ public class FastDFSClient {
         StorageClient storageClient = null;
 
         try {
+            // 获取存储客户端
             storageClient = getStorageClient();
+            // 文件上传
             storageClient.upload_file(file.getContent(), file.getExt(), meta_list);
         } catch (IOException e) {
             logger.error("IO Exception when uploadind the file:" + file.getName(), e);
@@ -56,15 +60,82 @@ public class FastDFSClient {
         return uploadResults;
     }
 
+    /**
+     * 获取文件
+     * @param groupName
+     * @param remoteFileName
+     * @return
+     */
+    public static FileInfo getFile(String groupName, String remoteFileName) {
+        try {
+            StorageClient storageClient = getStorageClient();
+            return storageClient.get_file_info(groupName,remoteFileName);
+        }catch (IOException e) {
+            logger.error("IO Exception: get File form fast dfs failed", e);
+        }catch (Exception e) {
+            logger.error("Non IO Exception: Get File from Fast DFS failed", e);
+        }
+
+
+
+        return null;
+    }
+
+    /**
+     * 下载文件
+     * @param groupName
+     * @param remoteFileName
+     * @return
+     */
+    public static InputStream downFile(String groupName, String remoteFileName) {
+        try {
+            StorageClient storageClient = getStorageClient();
+            byte[] fileByte = storageClient.download_file(groupName, remoteFileName);
+            InputStream ins = new ByteArrayInputStream(fileByte);
+
+            return ins;
+        } catch (IOException e) {
+            logger.error("IO Exception: get File form fast dfs failed", e);
+        } catch (Exception e) {
+            logger.error("Non IO Exception: Get File from Fast DFS failed", e);
+        }
+        return null;
+    }
+
+    /**
+     * 删除文件
+     * @param groupName
+     * @param remoteFileName
+     */
+    public static void deleteFile(String groupName, String remoteFileName) throws Exception{
+        StorageClient storageClient = getStorageClient();
+        int i = storageClient.delete_file(groupName, remoteFileName);
+        logger.info("delete file successfully" + i);
+    }
+
+    /**
+     * 获取StorageClient客户端
+     * @return
+     * @throws IOException
+     */
     private static StorageClient getStorageClient() throws IOException{
-        TrackerServer trackServer = getTrackServer();
+        TrackerServer trackServer = getTrackerServer();
         StorageClient storageClient = new StorageClient(trackServer, null);
         return storageClient;
     }
 
-    private static TrackerServer getTrackServer() throws IOException {
+    /**
+     * 获取追踪服务器TrackServer
+     * @return
+     * @throws IOException
+     */
+    private static TrackerServer getTrackerServer() throws IOException {
         TrackerClient trackerClient = new TrackerClient();
         TrackerServer trackerServer = trackerClient.getConnection();
         return trackerServer;
+    }
+
+    public static String getTrackerUrl() throws IOException {
+        return "http://"+getTrackerServer().getInetSocketAddress().getHostString()+":"+ClientGlobal.getG_tracker_http_port()+"/";
     }
 }
--- src/main/java/com/wayne/partone/web/UploadController.java
+++ src/main/java/com/wayne/partone/web/UploadController.java
@@ -1,5 +1,9 @@
 package com.wayne.partone.web;
 
+import com.wayne.partone.fastdfs.FastDFSClient;
+import com.wayne.partone.fastdfs.FastDFSFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,7 +21,7 @@ import java.nio.file.Paths;
 @Controller
 public class UploadController {
     private static String UPLOADED_FOLDER = "/Users/fantuan/Downloads/";
-
+    private static Logger logger = LoggerFactory.getLogger(UploadController.class);
     /**
      * 单文件上传页面
      * @return
@@ -48,12 +53,17 @@ public class UploadController {
             return "redirect:/uploadStatus";
         }
         try {
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
-            Files.write(path, bytes);
+//            byte[] bytes = file.getBytes();
+//            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+//            Files.write(path, bytes);
+
+            // 使用FastDFS文件系统上传文件
+            String path = saveFile(file);
 
             redirectAttributes.addFlashAttribute("message",
                     "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+            redirectAttributes.addFlashAttribute("path", "file path url '" + path + "'");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -85,5 +95,39 @@ public class UploadController {
         }
         redirectAttributes.addFlashAttribute("message", "You successfully uploaded all");
         return "redirect:/uploadStatus";
+    }
+
+    /**
+     * 上传控制
+     * @param multipartFile
+     * @return
+     * @throws IOException
+     */
+    public String saveFile(MultipartFile multipartFile) throws IOException {
+        String[] fileAbsolutePath= {};
+        String fileName = multipartFile.getOriginalFilename();
+        String ext = fileName.substring(fileName.lastIndexOf(".") + 1);
+        byte[] file_buff = null;
+        InputStream inputStream = multipartFile.getInputStream();
+        if (inputStream !=null) {
+            int len1 = inputStream.available();
+            file_buff = new byte[len1];
+            inputStream.read(file_buff);
+        }
+        inputStream.close();
+        FastDFSFile file = new FastDFSFile(fileName, file_buff, ext);
+
+        try {
+            fileAbsolutePath= FastDFSClient.upload(file);
+        }catch (Exception e) {
+            logger.error("upload file Exception!",e);
+        }
+
+        if (fileAbsolutePath==null) {
+            logger.error("upload file failed,please upload again!");
+        }
+
+        String path = FastDFSClient.getTrackerUrl() + fileAbsolutePath[0] + "/" + fileAbsolutePath[1];
+        return path;
     }
 }
